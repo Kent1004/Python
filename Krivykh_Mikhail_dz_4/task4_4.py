d51eb71b10346ead28d2d46b5022dbad20b6e674
Mikhail K <ballbes@mail.ru>
2021-04-07 01:30:03 +0300

    task#4

---
+++ Krivykh_Mikhail_dz_4/task4_4.py
@@ -0,0 +1,10 @@
+"""Импортируем модуль utils """
+import util
+
+"""делаем запросы курса различных валют"""
+
+util.currency_rates('usd')
+util.currency_rates ('gbp')
+util.currency_rates ('INR')
+util.currency_rates  ('KZT')
+util.currency_rates ('HKD')
---
+++ Krivykh_Mikhail_dz_4/util.py
@@ -0,0 +1,15 @@
+import requests
+from datetime import date
+from datetime import datetime
+
+"""Функция вывода курса введенной валюты и даты выставления курса валюты """
+def currency_rates (currency):
+   """Делаем запрос валют на сайт ЦБ РФ"""
+   r = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
+   """Получаем дату выставления курса ЦБ РФ через поиск и срез"""
+   dateRequest = r.text[r.text.find('Date="'):]
+   currencyData = datetime.strptime((dateRequest[(dateRequest.find('Date') + 6):(dateRequest.find('Date') + 16)]), "%d.%m.%Y").date()
+   """Получаем курс валюты  через поиск и срез"""
+   currencyRequest = r.text[r.text.find(currency.upper()):]
+   currencyRequest = round(float(currencyRequest[(currencyRequest.find('<Value>')+7):(currencyRequest.find('</Value>'))].replace(',','.')),2)
+   print (f'{currencyRequest} , {currencyData}')
